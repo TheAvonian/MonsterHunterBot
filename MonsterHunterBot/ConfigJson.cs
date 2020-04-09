af4c031229bc8d558f9822e9ae330e99d09db077
TheAvonian <misteraveso@gmail.com>
2020-04-09 02:48:08 -0400

    Add project files.

---
+++ MonsterHunterBot/ConfigJson.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonsterHunterBot
+{
+    public struct ConfigJson
+    {
+        [JsonProperty("Token")]
+        public string Token { get; private set; }
+        [JsonProperty("Prefix")]
+        public string Prefix { get; private set; }
+    }
+}
