d01782a568feb1cdf22ce2f208a8dd15afb5a6e5
Justin Kennedy <kennedjustin7@gmail.com>
2020-04-09 22:53:32 -0400

    Added the pong command to test justin's competence

---
+++ MonsterHunterBot/Commands/BasicCommands.cs
@@ -0,0 +1,18 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterHunterBot.Commands
+{
+    public class BasicCommands : BaseCommandModule
+    {
+        [Command("Ping")]
+        public async Task Ping(CommandContext ctx)
+        {
+            await ctx.Channel.SendMessageAsync("Pong").ConfigureAwait(false);
+        }
+    }
+}
