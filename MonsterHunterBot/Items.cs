cd8b60e42691df6d7e381de626fac53f8ee00696
Justin Kennedy <kennedjustin7@gmail.com>
2020-04-10 17:26:09 -0400

    Added item abstract class

---
+++ MonsterHunterBot/Items.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonsterHunterBot
+{
+    public abstract class Items
+    {
+        public string Name; 
+
+
+        public Items()
+        {
+
+        }
+    }
+}
